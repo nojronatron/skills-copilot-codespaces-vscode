9b2d8d3ad4ac8720386da7c58a73fcdc1490a0c5
Jon Rumsey <nojronatron@users.noreply.github.com>
2024-05-27 15:43:15 +0000

    Copilot third commit, third try

--- comments.js
+++ comments.js
@@ -17,25 +17,3 @@ app.get('/comments', function(req, res) {
 app.listen(3000, function() {
   console.log('Server started');
 });
-
-// Path: index.html
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Comments</title>
-</head>
-<body>
-  <h1>Comments</h1>
-  <ul id="comments"></ul>
-  <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      $.getJSON('http://localhost:3000/comments', function(comments) {
-        comments.forEach(function(comment) {
-          $('#comments').append('<li>' + comment.name + ': ' + comment.message + '</li>');
-        });
-      });
-    });
-  </script>
-</body>
-</html>
